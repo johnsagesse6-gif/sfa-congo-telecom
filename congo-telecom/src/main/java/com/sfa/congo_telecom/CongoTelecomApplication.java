dd7f12b06acf8548ae0ca10e370af7dd6e5294c2
John_sagesse <johnsagesse6@gmail.com>
2026-04-06 15:11:06 +0100

    Initialisation du projet SFA Congo Telecom et structure Maven

---
+++ congo-telecom/src/main/java/com/sfa/congo_telecom/CongoTelecomApplication.java
@@ -0,0 +1,13 @@
+package com.sfa.congo_telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongoTelecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongoTelecomApplication.class, args);
+	}
+
+}
---
+++ congo-telecom/src/test/java/com/sfa/congo_telecom/CongoTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfa.congo_telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongoTelecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
